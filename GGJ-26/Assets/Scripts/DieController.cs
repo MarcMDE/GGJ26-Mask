d984dd1f35ab5c936d569dc5d9ced41f4c8f50ea
Guillermocidm <guillermocidm@gmail.com>
2026-01-31 22:27:46 +0100

    Kill everyone donete

---
+++ GGJ-26/Assets/Scripts/DieController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using UnityEngine;
+
+public class DieController : MonoBehaviour
+{
+    private Animator animator;
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void Die()
+    {
+        animator.SetTrigger("DieTrigger");
+        StartCoroutine(DieCoroutine());
+    }
+
+    private IEnumerator DieCoroutine()
+    {
+        yield return new WaitUntil(() => animator.GetCurrentAnimatorStateInfo(0).IsName("Die"));
+        animator.SetBool("isDead", true);
+        yield return new WaitUntil(() => !animator.GetCurrentAnimatorStateInfo(0).IsName("Die"));
+        Destroy(gameObject);
+    }
+}
