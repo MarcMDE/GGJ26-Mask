8247ee769b4729fccfafbaf8e5111bdc7f789b38
Guillermocidm <guillermocidm@gmail.com>
2026-02-01 10:58:42 +0100

    Switch mask material instead of model

--- GGJ-26/Assets/Scripts/ModelController.cs
+++ GGJ-26/Assets/Scripts/ModelController.cs
@@ -2,16 +2,15 @@ using UnityEngine;
 
 public class ModelController : MonoBehaviour
 {
-    [SerializeField] private GameObject[] models;
+    [SerializeField] private Material[] modelsMaterials;
+    [SerializeField] private GameObject maskModel;
 
     int currentFaction = 0;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
@@ -24,12 +23,10 @@ public class ModelController : MonoBehaviour
 
     public void SetFaction(int setFaction)
     {
-        if (setFaction >= models.Length) return;
+        if (setFaction >= modelsMaterials.Length) return;
 
         currentFaction = setFaction;
-
-        for (int i = 0; i < models.Length; i++) {
-            models[i].SetActive(currentFaction == i);
-        }
+        maskModel.GetComponent<Renderer>().material = modelsMaterials[currentFaction];
+        
     }
 }
