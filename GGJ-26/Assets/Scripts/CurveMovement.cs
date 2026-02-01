e124439b17f717e787c7b732bd644e159d83c8c4
Javi-HA <61690146+Javi-HA@users.noreply.github.com>
2026-02-01 16:44:58 +0100

    fog

--- GGJ-26/Assets/Scripts/CurveMovement.cs
+++ GGJ-26/Assets/Scripts/CurveMovement.cs
@@ -5,16 +5,19 @@ public class CurveMovement : MonoBehaviour
     [SerializeField] AnimationCurve curve;
     [SerializeField] private float strength;
     [SerializeField] private float speedFactor;
+
+    private float offset = 0f;
     private Vector3 ogPosition;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         ogPosition = transform.position;
+        offset = Random.Range(0, 1);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = ogPosition + transform.forward * strength * curve.Evaluate(Time.time* speedFactor);
+        transform.position = ogPosition + transform.forward * strength * curve.Evaluate(Time.time* speedFactor + offset);
     }
 }
