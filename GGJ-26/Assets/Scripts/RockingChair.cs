c9505313bd2facd258de427166dd66e2aefb1bdd
Guillermocidm <guillermocidm@gmail.com>
2026-02-01 16:15:20 +0100

    Rocking Chair Script

---
+++ GGJ-26/Assets/Scripts/RockingChair.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using System.Collections;
+using System;
+using UnityEngine;
+
+public class RockingChair : MonoBehaviour
+{
+    [SerializeField] private float rockingSpeed = 1f;
+    [SerializeField] private float rockingAngle = 15f;
+
+    private Quaternion initialRotation;
+
+    void Start()
+    {
+        initialRotation = transform.rotation;
+    }
+
+    void Update()
+    {
+        float angle = Mathf.Sin(Time.time * rockingSpeed) * rockingAngle;
+        transform.rotation = initialRotation * Quaternion.Euler(angle, 0, 0);
+    }
+}
